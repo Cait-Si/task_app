d9634a91620755aa1397c73291df98cd4007a7ea
Keito <cait@KeitonoMacBook-Air.local>
2021-10-28 17:45:18 +0900

    CreateTaskApp

---
+++ db/migrate/20211027002602_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title, null: false
+      t.date :start_date, null: false
+      t.date :end_date, null: false
+      t.boolean :all_day
+
+      t.timestamps
+    end
+  end
+end
